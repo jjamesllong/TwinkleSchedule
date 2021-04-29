2c80eec0d69b1d5f4f3326720181a7332202b95e
arthur <524270005@qq.com>
2021-04-29 12:52:49 +0800

    added test data

--- app/src/main/java/com/keycome/twinkleschedule/database/TestData.kt
+++ app/src/main/java/com/keycome/twinkleschedule/database/TestData.kt
@@ -13,5 +13,183 @@ class TestData {
             duration = 45,
             timeLine = timeLineArray
         )
+        private val course = CourseEntity(
+            courseId = 0,
+            parentScheduleId = 0,
+            title = "",
+            day = Day.Monday,
+            section = Section(1, 2),
+            week = Week(1, 18),
+            continuity = Continuity.Continuous,
+            teacher = "",
+            classroom = ""
+        )
+        val courseArray = arrayOf(
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "马克思主义基本原理概论",
+                day = Day.Monday,
+                section = Section(1, 2),
+                week = Week(3, 14),
+                continuity = Continuity.Continuous,
+                teacher = "袁夫友",
+                classroom = "文通205"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "有氧健身俱乐部",
+                day = Day.Monday,
+                section = Section(3, 4),
+                week = Week(1, 16),
+                continuity = Continuity.Continuous,
+                teacher = "陆守芹",
+                classroom = "第二田径场(东)"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "信号与系统",
+                day = Day.Monday,
+                section = Section(5, 7),
+                week = Week(3, 15),
+                continuity = Continuity.Continuous,
+                teacher = "施彩平",
+                classroom = "文华103"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "海洋世界",
+                day = Day.Monday,
+                section = Section(8, 9),
+                week = Week(3, 14),
+                continuity = Continuity.Continuous,
+                teacher = "苏振霞",
+                classroom = "文华105"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "电磁场与电磁波",
+                day = Day.Tuesday,
+                section = Section(1, 2),
+                week = Week(4, 14),
+                continuity = Continuity.Continuous,
+                teacher = "张之光",
+                classroom = "主楼208"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "大学英语(四)",
+                day = Day.Tuesday,
+                section = Section(3, 4),
+                week = Week(3, 14),
+                continuity = Continuity.Continuous,
+                teacher = "刘伟",
+                classroom = "文通119"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "数字电子技术B",
+                day = Day.Tuesday,
+                section = Section(5, 7),
+                week = Week(3, 15),
+                continuity = Continuity.Continuous,
+                teacher = "栾晓东",
+                classroom = "文华102"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "马克思主义基本原理概论",
+                day = Day.Wednesday,
+                section = Section(1, 2),
+                week = Week(3, 14),
+                continuity = Continuity.Continuous,
+                teacher = "袁夫友",
+                classroom = "文通205"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "数值计算",
+                day = Day.Wednesday,
+                section = Section(3, 4),
+                week = Week(3, 10),
+                continuity = Continuity.Continuous,
+                teacher = "李步军",
+                classroom = "文通213"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "形势与政策",
+                day = Day.Wednesday,
+                section = Section(5, 6),
+                week = Week(8, 10),
+                continuity = Continuity.Even,
+                teacher = "丁小青",
+                classroom = "文通302"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "信号与系统",
+                day = Day.Thursday,
+                section = Section(1, 2),
+                week = Week(3, 15),
+                continuity = Continuity.Continuous,
+                teacher = "施彩平",
+                classroom = "文华103"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "电磁场与电磁波",
+                day = Day.Thursday,
+                section = Section(3, 4),
+                week = Week(3, 14),
+                continuity = Continuity.Continuous,
+                teacher = "张之光",
+                classroom = "主楼208"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "大学英语(四)",
+                day = Day.Thursday,
+                section = Section(5, 6),
+                week = Week(3, 14),
+                continuity = Continuity.Continuous,
+                teacher = "刘伟",
+                classroom = "文通119"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "数值计算",
+                day = Day.Friday,
+                section = Section(1, 2),
+                week = Week(3, 10),
+                continuity = Continuity.Continuous,
+                teacher = "李步军",
+                classroom = "文通213"
+            ),
+            CourseEntity(
+                courseId = 0,
+                parentScheduleId = 0,
+                title = "数字电子技术B",
+                day = Day.Friday,
+                section = Section(3, 4),
+                week = Week(3, 15),
+                continuity = Continuity.Continuous,
+                teacher = "栾晓东",
+                classroom = "文华102"
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/keycome/twinkleschedule/model/CourseBlock.kt
@@ -0,0 +1,7 @@
+package com.keycome.twinkleschedule.model
+
+data class CourseBlock(
+    val isPlaceHolder: Boolean,
+    val spanSize: Int,
+    val courseIndex: Int = -1
+)
