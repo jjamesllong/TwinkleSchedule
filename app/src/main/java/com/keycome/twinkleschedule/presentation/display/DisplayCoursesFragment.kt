c3d84aa12e7e4771a4c6c4a6ca3e55c3446a4ddb
arthur <524270005@qq.com>
2021-05-02 17:24:52 +0800

    revert to last commit
    this reverts commit e466209f8d439801f9e29a6311b0a08de4d12611.

--- app/src/main/java/com/keycome/twinkleschedule/presentation/display/DisplayCoursesFragment.kt
+++ app/src/main/java/com/keycome/twinkleschedule/presentation/display/DisplayCoursesFragment.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.GridLayout
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import com.keycome.twinkleschedule.database.Day
+import androidx.recyclerview.widget.GridLayoutManager
 import com.keycome.twinkleschedule.database.TestData
 import com.keycome.twinkleschedule.databinding.FragmentDisplayCoursesBinding
 
@@ -25,32 +22,15 @@ class DisplayCoursesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val courseArray = TestData.courseArray
-        binding.gridLayout.apply { rowCount = TestData.courseSchedule.courses; columnCount = 5 }
-        lifecycleScope.launchWhenCreated {
-            for (course in courseArray) {
-                val params = GridLayout.LayoutParams().apply { width = 0; height = 0 }
-                val text = TextView(requireContext())
-                params.rowSpec = GridLayout.spec(
-                    course.section.startSection - 1,
-                    course.section.endSection - course.section.startSection + 1,
-                    1f
-                )
-                params.columnSpec = GridLayout.spec(
-                    convertDayToColumn(course.day), 1, 1f
-                )
-                text.text = course.title
-                binding.gridLayout.addView(text, params)
-            }
+        val gridLayoutManager = GridLayoutManager(
+            context,
+            TestData.courseSchedule.courses,
+            GridLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.courseRecyclerView.apply {
+            layoutManager = gridLayoutManager
+            adapter = CourseAdapter()
         }
-    }
-
-    private fun convertDayToColumn(day: Day) = when (day) {
-        Day.Monday -> 0
-        Day.Tuesday -> 1
-        Day.Wednesday -> 2
-        Day.Thursday -> 3
-        Day.Friday -> 4
-        else -> -1
     }
 }
